93f54d45ce458f406ee3247171b299cb9fd6ab7a
Gipernova <lia_shongmao@mail.ru>
2022-03-14 09:55:05 +0300

    comm -async key

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/wrapperspb"
+	"log"
 	"os"
 	"time"
 )
@@ -15,7 +16,7 @@ func main() {
 	cwt, _ := context.WithTimeout(context.Background(), time.Second*5)
 	conn, err := grpc.DialContext(cwt, "localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		panic(err)
+		log.Fatalf("error grpc connection %s", err)
 	}
 	defer conn.Close()
 
@@ -32,17 +33,35 @@ func main() {
 
 	flag.Parse()
 	if *async {
-		go func() {
-			stream, _ := client.DownloadAsync(context.Background())
-			if err := stream.Send(&wrapperspb.StringValue{Value: linc[1]}); err != nil {
-				panic(err)
-			}
-		}()
+		//stream, _ := client.DownloadAsync(context.Background())
+		//for _, value := range linc[1:] {
+		//	if err := stream.Send(&wrapperspb.StringValue{Value: value}); err != nil {
+		//		log.Fatalf("err to send: %s", err)
+		//	}
+		//}
+		//channel := make(chan struct{})
+		//go asncDownloadBdrectionalRPC(stream, channel)
+		//if err := stream.CloseSend(); err != nil {
+		//	log.Fatal(err)
+		//}
+		//<-channel
 	} else {
 		image, err := client.Download(context.Background(), &wrapperspb.StringValue{Value: linc[1]})
 		if err != nil {
-			panic(err)
+			log.Fatalf("error from download func %s", err)
 		}
-		fmt.Println(image.String())
+		fmt.Println(image.GetStatus(), image.GetId())
 	}
 }
+
+//
+//func asncDownloadBdrectionalRPC(stream proto.Thumbnails_DownloadAsyncClient, c chan struct{}) {
+//	for {
+//		images, err := stream.Recv()
+//		if err == io.EOF {
+//			break
+//		}
+//		fmt.Println(images.GetStatus(), images.GetId())
+//	}
+//	<-c
+//}
